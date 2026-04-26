7a254575df55a4566280a206a858d12a59d46af5
Raz <raz@newbeach.co>
2026-04-26 22:52:20 +0800

    update

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -19,7 +19,7 @@ export const Hero: React.FC = () => {
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-blue-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-2 w-2 bg-blue-500"></span>
           </span>
-          Currently available for new projects
+          Available for full-time employment
         </motion.div>
 
         {/* Headline */}
